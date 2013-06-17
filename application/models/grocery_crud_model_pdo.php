cb29ec634b5a62a13780382704c63de38297ab0d
GoFrendi Gunawan <gofrendiasgard@gmail.com>
2013-06-18 06:02:39 +0700

    add examples controller and add grocery_crud_generic_model

--- application/models/grocery_crud_model_pdo.php
+++ application/models/grocery_crud_model_pdo.php
@@ -1,4 +1,7 @@
 <?php
-class grocery_crud_model_PDO extends grocery_CRUD_Model{
-	
+class grocery_crud_model_PDO extends grocery_CRUD_Generic_Model{
+    public function __construct(){
+        parent::__construct();
+        $this->subdriver = $this->db->subdriver;
+    }
 }
